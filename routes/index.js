84387bfebd1a3a66a60449d6964b8217219d1acd
diananov11 <ohlemon0678@gmail.com>
2024-10-16 23:28:49 +0700

    add informasi res when hit /

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,13 @@ const authRoute = require("./auth-route");
 const todoRoute = require("./todo-route");
 const studentRoute = require("./student-route");
 
+route.get("/", (req, res) => {
+  res.json({
+    message:
+      "please see the documentation at https://github.com/diananov11/backend-todo",
+  });
+});
+
 route.use("/auth", authRoute);
 route.use("/todos", todoRoute);
 route.use("/students", studentRoute);
